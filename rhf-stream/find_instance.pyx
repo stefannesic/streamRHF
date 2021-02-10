1dadec0bfa8467dc01530c98076dbcba1b6a0ba5
Stefan Nesic <stefan.nesic@protonmail.com>
2021-02-10 01:34:32 +0100

    Add indexing to anomaly scoring

--- rhf-stream/find_instance.pyx
+++ rhf-stream/find_instance.pyx
@@ -1,19 +1,19 @@
 from my_imports import np, Node
 # returns Leaf of instance in a RHT
-cpdef find_instance(rht, x):
+cpdef find_instance(rht, int x, x_val):
     cdef float a_val
     cdef Py_ssize_t a
     if rht.left != None and rht.right != None:
         # not leaf
         a = rht.attribute
         a_val = rht.value
-        if (x[a] < a_val):
-            return find_instance(rht.left, x)
+        if (x_val[a] < a_val):
+            return find_instance(rht.left, x, x_val)
         else:
-            return find_instance(rht.right, x)
+            return find_instance(rht.right, x, x_val)
     else:
         # leaf
-        data = Node.data_complete[np.asarray(rht.data)]
+        data = np.asarray(rht.data)
         if x in data:
             return rht.data.size
 
