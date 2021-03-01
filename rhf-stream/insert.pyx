14b39a7994646b1ee98f80bc72e35e66e6aaf9ba
Stefan Nesic <stefan.nesic@protonmail.com>
2021-03-01 16:14:35 +0100

    Time profiling the insertion function

--- rhf-stream/insert.pyx
+++ rhf-stream/insert.pyx
@@ -1,34 +1,41 @@
-from my_imports import Node, np, rht, ks_cy
+from my_imports import Node, np, rht, ks_cy, time
 # inserts new data point in leaf
 # returns updated tree if splits in path affected by new data point
 def insert(root, x):
     # analyze non leaf node until x is inserted
     # if a non leaf node kurtosis changes, recalculate split
+    t0 = time.time()
     x_value = Node.data_complete[x]
     if (x_value.size == 0):
         print("X value is empty and x=", x)
     # moments for tree recalculations
     moments0 = np.zeros([x_value.size,5], dtype=np.float32)
+    t1 = time.time()
+    print("before 1st if =", t1 - t0)
     # tree is not leaf
     if root.left != None and root.right != None:
         # ----- check for resplit ------
         # calculate new kurtosis and sum with new data point
         root.data = np.append(root.data, x, axis=0) 
-       
+        t2 = time.time()
+        print("after append data=", t2 - t1)
         new_ks, new_k, root.moments = ks_cy.kurtosis_sum(x_value, root.moments)
-
+        t3 = time.time()
+        print("after ks sum=", t3 - t2)
         # the previously stored probability of splitting on a 
         old_p = np.asarray(root.old_k) / root.old_ks
         # the current probability
         new_p = np.asarray(new_k) / new_ks
         delta_p = new_p - old_p 
-      
+        t4 = time.time()
+        print("delta_p calc=", t4 - t3)
         for a in np.where(abs(delta_p) >= 1 - rht.eps)[0]:
             if ((root.attribute != a and delta_p[a] > 0) or (root.attribute == a and delta_p[a] < 0)):                       
                 # node is replaced by a new tree 
                 Node.rebuild[root.nd] += 1
                 return rht.rht(root.data, root.nd, moments0) 
-        
+        t5 = time.time()
+        print("no change > eps time=", t5 - t4)
         # ----- insertion ------- 
 
         a = root.attribute
@@ -42,18 +49,24 @@ def insert(root, x):
             child = root.left
         else:
             child = root.right
-        
+        t6 = time.time()
+        print("find child time=", t6 - t5)
         # child is leaf  
         if child.left == None:
             # leaf is at max depth
             if child.nd == Node.H:
                 child.insertData(x)
                 root.replace(child, left)
+                t7 = time.time()
+                print("insertData and replace child time=", t7 - t6)
             else:
                 # leaf depth is not max, so new split
                 Node.rebuild[5] += 1
                 root.replace(rht.rht(np.append(child.data, x, axis=0), child.nd, moments0), left)
-                
+                t8 = time.time()
+                print("leaf not max depth split time=", t8 - t6)
+
+
         else:
             # keep searching
             root.replace(insert(child, x), left)
