15652473ce748986f9c10d0afc1aec2632aed764
Stefan Nesic <stefan.nesic@protonmail.com>
2021-03-24 14:46:11 +0100

    Measure time for loop content vs. loop itself

--- rhf-stream/kurtosis_sum.pyx
+++ rhf-stream/kurtosis_sum.pyx
@@ -9,7 +9,7 @@ def same_values(float[:] arr):
             return False
     return True 
 
-# msum of log(Kurtosis(X[a] + 1)) of attributes 0 to d inclusive
+# sum of log(Kurtosis(X[a] + 1)) of attributes 0 to d inclusive
 # when the function is used for insertions, insert_mode is True
 def kurtosis_sum(float[:,:] X, moments, bint insert_mode=False):
     t0 = time.time()
@@ -19,7 +19,7 @@ def kurtosis_sum(float[:,:] X, moments, bint insert_mode=False):
     cdef Py_ssize_t d = X.shape[1]
     cdef int a
     cdef float[:] kurt = np.empty([d], np.float32)
-    cdef float[:] simpleX
+    cdef float[:] X_0
     if insert_mode:
         X_0 = X[0]
     if (n_elems == 0):
@@ -30,34 +30,37 @@ def kurtosis_sum(float[:,:] X, moments, bint insert_mode=False):
     t2 = time.time()
     # loop over the transpose matrix in order to analyze by column
     for a in range(0, d):
-        t2b = time.time()
+        t4 = time.time() 
         # if we're not in insertion mode, calculate if all of the values are the same
         if not(insert_mode):
             samevals = not(same_values(X[:,a]))
         else: 
             samevals = False
         
-        t2c = time.time()
-        Node.ksstats[1] += (t2c - t2b)
-        
         # samevals is always false in insert_mode so only the second part matters 
         if (samevals or (insert_mode and (moments[a][5] == 0 or moments[a][0] != X_0[a]))):
-            t2d = time.time() 
-            Node.ksstats[2] += (t2d - t2c)
+            t6 = time.time()
             kurt[a], moments[a] = ik.incr_kurtosis(X[:,a], moments[a])
+            t7 = time.time()
+            # time for incremental function
+            Node.ksstats[2] += (t7- t6)        
             kurt[a] = log(kurt[a] + 1)
             summ += kurt[a]
-            t2e = time.time()
-            Node.ksstats[3] += (t2e - t2d)            
         else:
-            t2d = time.time()
-            Node.ksstats[2] += (t2d - t2c)
             if not(insert_mode):
                 # label the column as having same values for future incr. checking
                 moments[a][5] = 1
             kurt[a] = 0
             # increase number of elements
             moments[a][4] += 1
+        t5 = time.time()
+        # measure the time of one iteration of the for loop
+        if (a == 0):
+            Node.ksstats[1] += (t4-t2)
+        # measure the total time of the loop contents 
+        Node.ksstats[3] += (t5-t4)
+    # time entire for loop
     t3 = time.time()
     Node.ksstats[4] += (t3 - t2)  
     return summ, kurt, moments
+
