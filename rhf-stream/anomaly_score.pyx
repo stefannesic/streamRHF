f652640ec1d87588b2a76187c07fc0e8a72985ac
Stefan Nesic <stefan.nesic@protonmail.com>
2021-04-07 23:43:25 +0200

    Initial implementation runs but AP is not good except for cardio (>0.9)

--- rhf-stream/anomaly_score.pyx
+++ rhf-stream/anomaly_score.pyx
@@ -1,15 +1,19 @@
-from my_imports import fi, np
+from my_imports import np, constants, dataset
 from libc.math cimport log
 
-cpdef anomaly_score(rhf, int n, int x, float[:] x_value):
-    cdef float sum = 0.0
-    cdef int leaf_size
+cpdef anomaly_score():
     cdef float p
-    cdef Py_ssize_t tsize = rhf.size 
-    
-    for i in range(0, tsize):
-        # number of distinct instances in the leaf of the given instance
-        leaf_size = fi.find_instance(rhf[i], x, x_value)
-        p = leaf_size / n
-        sum += log(1 / p)
-    return sum
+    cdef int leaf_size, i, j
+    cdef Py_ssize_t t = constants.T, n = constants.N
+    cdef float[:] scores = np.zeros([n], np.float32)
+    cdef int[:] elem
+
+    for i in range (0, constants.T):
+        for j in range(0, constants.N):
+            elem = dataset.index[i][j]
+            data_pointer = elem[0]
+            leaf_size = elem[1]
+            p = leaf_size / n
+            scores[j] += log(1 / p)
+    return scores
+
