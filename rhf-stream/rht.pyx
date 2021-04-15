5c41209c5adea02805918ed9eae18271ea416353
Stefan Nesic <stefan.nesic@protonmail.com>
2021-04-15 11:32:24 +0200

    New data structure for insertion initialized in batch algorithm

--- rhf-stream/rht.pyx
+++ rhf-stream/rht.pyx
@@ -16,14 +16,14 @@ def sort(float[:,:] data, int[:,:] indexes, int start, int end, int a, float a_v
         indexes[j][0] = temp
     return j
          
-def rht(float[:,:] data, int[:,:] indexes, split_info, float[:,:,:] moments, int start, int end, int nd, int H, int nodeID=0):
+def rht(float[:,:] data, int[:,:] indexes, insertionDS, split_info, float[:,:,:] moments, int start, int end, int nd, int H, int nodeID=0):
     cdef int ls, a, split
     cdef float ks, a_val 
     cdef float[:] kurt
     cdef float[:,:] moments_res
     if (end == start or nd >= H):
         # leaf size
-        indexes = fill_leaf(indexes, start, end)
+        indexes = fill_leaf(indexes, insertionDS, nodeID, nd, H, start, end) 
     else:
         # calculate kurtosis
         ks, kurt, moments_res = ks_cy.kurtosis_sum(data, indexes, moments[nodeID], start, end)
@@ -41,13 +41,31 @@ def rht(float[:,:] data, int[:,:] indexes, split_info, float[:,:,:] moments, int
             split_info.splits[nodeID] = split
             split_info.attributes[nodeID] = a
             split_info.values[nodeID] = a_val
-            rht(data, indexes, split_info, moments, start, split-1, nd+1, H, nodeID=2*nodeID+1)
-            rht(data, indexes, split_info, moments, split, end, nd+1, H, nodeID=2*nodeID+2)
+            rht(data, indexes, insertionDS, split_info, moments, start, split-1, nd+1, H, nodeID=2*nodeID+1)
+            rht(data, indexes, insertionDS, split_info, moments, split, end, nd+1, H, nodeID=2*nodeID+2)
            
 
-def fill_leaf(int[:,:] indexes, int start, int end):
-    cdef int ls, i
+def fill_leaf(int[:,:] indexes, insertionDS, int nodeID, int nd, int H, int start, int end):
+    cdef int ls, i, leaf_index
     ls = end - start + 1
-    for i in range(start, end+1):
+
+    # calculate index of insertionDS 
+    if nd >= (2**H - 1) : # leaf is at max depth
+        leaf_index = nodeID
+    else:
+        leaf_index = 2**(H-nd)*nodeID + 2**(H-nd) - 1
+    
+    # at this point, leaf_index is nodeID and needs to be adjusted for indexing
+    leaf_index = nodeID - (2**H)
+
+    for i in range(start, end+1):  
+        # store leaf in insertion DS
+        # fetch counter
+        counter = insertionDS.counters[leaf_index]
+        # get the actual pointer to the dataset indexes[k][0]
+        insertionDS.table[leaf_index][counter] = indexes[i][0]
+        # increment counter
+        insertionDS.counters[leaf_index] += 1
+
         indexes[i][1] = ls
 
