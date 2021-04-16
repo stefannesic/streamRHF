1b2c88979076f4f018f02d45a6668b65a12925aa
Stefan Nesic <stefan.nesic@protonmail.com>
2021-04-16 16:12:19 +0200

    Fix issue with attribute selecting a_min = index[0] not index[start]

--- rhf-stream/get_attribute.pyx
+++ rhf-stream/get_attribute.pyx
@@ -11,8 +11,8 @@ def get_attribute(float[:,:] data, int[:,:] indexes, int start, int end, float k
     # the attribute is found in the bins of the cumulative sum of kurtoses 
     a = np.digitize(r, kurt, True)
     # get min and max
-    a_min = data[indexes[0][0]][a]
-    a_max = data[indexes[0][0]][a]
+    a_min = data[indexes[start][0]][a]
+    a_max = data[indexes[start][0]][a]
 
     for i in range(start, end+1):
             temp = data[indexes[i][0]][a]
