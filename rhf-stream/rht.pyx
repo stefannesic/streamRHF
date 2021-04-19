e1c840d393946b323458f5ff93abcf94e4d62255
Stefan Nesic <stefan.nesic@protonmail.com>
2021-04-19 15:37:54 +0200

    Fix insertionDS indexing issues

--- rhf-stream/rht.pyx
+++ rhf-stream/rht.pyx
@@ -23,14 +23,13 @@ def rht(float[:,:] data, int[:,:] indexes, insertionDS, split_info, float[:,:,:]
     cdef float[:,:] moments_res
     if (end == start or nd >= H):
         # leaf size
-        indexes = fill_leaf(indexes, insertionDS, nodeID, nd, H, start, end) 
+        fill_leaf(indexes, insertionDS, nodeID, nd, H, start, end) 
     else:
         # calculate kurtosis
         ks, kurt, moments_res = ks_cy.kurtosis_sum(data, indexes, moments[nodeID], start, end)
         moments[nodeID] = moments_res
         if (ks == 0): # stop if all elems are the same
-            for i in range(start, end+1):
-                indexes[i][1] = 1
+            fill_leaf(indexes, insertionDS, nodeID, nd, H, start, end, 1) 
 
         else: # split
             a, a_val = ga.get_attribute(data, indexes, start, end, ks, kurt)
@@ -41,22 +40,30 @@ def rht(float[:,:] data, int[:,:] indexes, insertionDS, split_info, float[:,:,:]
             split_info.splits[nodeID] = split
             split_info.attributes[nodeID] = a
             split_info.values[nodeID] = a_val
+            split_info.kurtosis_vals[nodeID] = kurt
+            split_info.kurtosis_sum[nodeID] = ks
             rht(data, indexes, insertionDS, split_info, moments, start, split-1, nd+1, H, nodeID=2*nodeID+1)
             rht(data, indexes, insertionDS, split_info, moments, split, end, nd+1, H, nodeID=2*nodeID+2)
            
 
-def fill_leaf(int[:,:] indexes, insertionDS, int nodeID, int nd, int H, int start, int end):
-    cdef int ls, i, leaf_index
-    ls = end - start + 1
+def fill_leaf(int[:,:] indexes, insertionDS, int nodeID, int nd, int H, int start, int end, int ls = 0):
+    cdef int i, leaf_index
+    # leaf size is not set, calculate it.
+    if ls == 0:
+        ls = end - start + 1
 
-    # calculate index of insertionDS 
-    if nd >= (2**H - 1) : # leaf is at max depth
+    # calculate index of insertionDS
+    print("start=", start)
+    print("end=", end)
+    print("nd=", nd)
+    print("nodeID", nodeID) 
+    if nodeID >= (2**H - 1) : # leaf is at max depth
         leaf_index = nodeID
     else:
-        leaf_index = 2**(H-nd)*nodeID + 2**(H-nd) - 1
+        leaf_index = (2**(H - nd))*(nodeID + 1) - 1
     
     # at this point, leaf_index is nodeID and needs to be adjusted for indexing
-    leaf_index = nodeID - (2**H)
+    leaf_index = leaf_index - ((2**H) - 1)
 
     for i in range(start, end+1):  
         # store leaf in insertion DS
