36f27c9ed9ff86d526ccea15caa4dbebef06abd3
Stefan Nesic <stefan.nesic@protonmail.com>
2021-05-12 11:22:19 +0200

    Bash scripts to test insertion and batch algorithms on multiple datasets

---
+++ rhf-stream/scripts/insertion.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+i=3;
+j=$#;
+pref=$1
+shift 1;
+suff=$1
+shift 1;
+while [ $i -le $j ] 
+do
+    eval "nohup nice python -u insertion_script.py $1 100 5 10 0 0 1 > ${pref}/${1}_infres_eps0_x10_${suff}.txt &";
+    i=$((i + 1));
+    shift 1;
+done
