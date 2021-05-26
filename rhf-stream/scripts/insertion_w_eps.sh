69c5731a67962d3a11897dc4a4a4ec1423c20106
Stefan Nesic <stefan.nesic@protonmail.com>
2021-05-26 02:43:24 +0200

    New insertion script for modifiable epsilon

---
+++ rhf-stream/scripts/insertion_w_eps.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+i=3;
+j=$#;
+pref=$1
+shift 1;
+suff=$1
+shift 1;
+eps=$1
+shift 1;
+while [ $i -le $j ] 
+do
+    eval "nohup nice python -u insertion_script.py $1 100 5 10 ${eps} 0 1 > ${pref}/${1}_infres_eps${eps}_x10_${suff}.txt &";
+    i=$((i + 1));
+    shift 1;
+done
