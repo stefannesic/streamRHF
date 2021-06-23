507b78f44115a6041e9835325de0fec81c52fb66
Stefan Nesic <stefan.nesic@protonmail.com>
2021-06-23 16:06:07 +0200

    Modify insertion scripts to take into account initsample parameter

--- rhf-stream/scripts/insertion.sh
+++ rhf-stream/scripts/insertion.sh
@@ -6,9 +6,11 @@ pref=$1
 shift 1;
 suff=$1
 shift 1;
+init=$1;
+shift 1;
 while [ $i -le $j ] 
 do
-    eval "nohup nice python3 -u insertion_script.py $1 100 5 10 0 0 1 > ${pref}/${1}_infres_eps0_x10_${suff}.txt &";
+    eval "nohup nice python3 -u insertion_script.py $1 100 5 10 0 0 1 ${init} > ${pref}/${1}_infres_init${init}_eps0_x10_${suff}.txt &";
     i=$((i + 1));
     shift 1;
 done
--- rhf-stream/scripts/insertion_w_eps.sh
+++ rhf-stream/scripts/insertion_w_eps.sh
@@ -8,9 +8,11 @@ suff=$1
 shift 1;
 eps=$1
 shift 1;
+init=$1;
+shift 1;
 while [ $i -le $j ] 
 do
-    eval "nohup nice python3 -u insertion_script.py $1 100 5 10 ${eps} 0 1 > ${pref}/${1}_infres_eps${eps}_x10_${suff}.txt &";
+    eval "nohup nice python3 -u insertion_script.py $1 100 5 10 ${eps} 0 1 ${init} > ${pref}/${1}_infres_init${init}_eps${eps}_x10_${suff}.txt &";
     i=$((i + 1));
     shift 1;
 done
