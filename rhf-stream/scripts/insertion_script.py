fc8e27948ae8d23c94baad819f08060544d1d374
Stefan Nesic <stefan.nesic@protonmail.com>
2021-06-29 16:54:32 +0200

    Remove epsilon from algorithm and split based on attribute changes with fixed r-values

--- rhf-stream/scripts/insertion_script.py
+++ rhf-stream/scripts/insertion_script.py
@@ -15,8 +15,8 @@ import utils
 #sys.stdout = open('out.log', 'w')
 #sys.stderr = sys.stdout
 
-if len(sys.argv) < 9:
-    print("Command: python3 insertion_script.py [dataset] [T] [H] [iterations] [EPS] [step] [end] [initsamplepercent]")
+if len(sys.argv) < 6:
+    print("Command: python3 insertion_script.py [dataset] [T] [H] [iterations] [initsamplepercent]")
     quit()
 
 fname = str(sys.argv[1])
@@ -27,25 +27,16 @@ T = int(sys.argv[2])
 H = int(sys.argv[3])
 N = data.shape[0]
 iterations = int(sys.argv[4])
-step = float(sys.argv[6])
-EPS = float(sys.argv[5])
-end = int(sys.argv[7])
-init = int(sys.argv[8])
+init = int(sys.argv[5])
 N_init_pts = int(round(data.shape[0] * (init / 100)))
 print("N_init_pts=", N_init_pts)
 
 data = data.copy(order='C')
 for m in range(0, iterations):
     print("Iteration=", m)
-    epsilon = EPS    
-    for j in range(0, end):
-        print("EPS=", epsilon)
-        # build info reinitialized
-        t0 = time.time()
-        scores = rhfs.rhf_stream(data, T, H, N_init_pts, epsilon) 
-        t1 = time.time()
-        print("AP=", average_precision_score(labels, scores))
-        print("time (whole)=", t1 - t0)
-        epsilon = epsilon + step
-
-
+    # build info reinitialized
+    t0 = time.time()
+    scores = rhfs.rhf_stream(data, T, H, N_init_pts) 
+    t1 = time.time()
+    print("AP=", average_precision_score(labels, scores))
+    print("time (whole)=", t1 - t0)
