ec70ae237143f326b39ae93dd7ff1616e212516d
Stefan Nesic <stefan.nesic@inria.fr>
2021-09-22 16:27:15 +0200

    Add insertion script for constant size window

---
+++ rhf-stream/scripts/insertion_const.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+i=4;
+j=$#;
+pref=$1
+shift 1;
+suff=$1
+shift 1;
+init=$1;
+shift 1;
+while [ $i -le $j ] 
+do
+    eval "nohup nice python3 -u insertion_script_const.py $1 100 5 10 ${init} > ${pref}/${1}_infres_init${init}_x10_${suff}.txt &";
+    i=$((i + 1));
+    shift 1;
+done
