19feacaefcedbf2593f59293e4c1f415b14c99fe
Stefan Nesic <stefan.nesic@inria.fr>
2021-10-31 19:02:53 +0100

    Remove python and cython implementations of rhf

--- rhf-cython/Leaf.pyx
+++
@@ -1,6 +0,0 @@
-from my_imports import Node
-# a Leaf is a Node that has all parameters set to None except for data which contains its instances
-class Leaf(Node.Node):
-    def __init__(self, float[:,:] data):
-        super().__init__(value=-1, attribute=-1, left=None, right=None)
-        self.data = data
