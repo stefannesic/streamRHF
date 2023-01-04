bacaf5984ae18891260a28afc2bca1f35db89107
Stefan Nesic <stefan.nesic@protonmail.com>
2023-01-04 14:48:50 +0100

    Add scripts for constant size window

---
+++ rhf-stream/scripts/insertion_script_const.py
@@ -0,0 +1,46 @@
+import sys 
+sys.path.insert(1, '../../datasets/forStefan/')
+sys.path.insert(2, '../')
+import time 
+from sklearn.metrics import average_precision_score
+import math
+import scipy.io as sio
+import scipy.stats as sstats
+import random
+import numpy as np
+import rhf_stream as rhfs
+import mat73
+import utils
+
+#sys.stdout = open('out.log', 'w')
+#sys.stderr = sys.stdout
+
+if len(sys.argv) < 6:
+    print("Command: python3 insertion_script.py [dataset] [T] [H] [iterations] [initsamplesize]")
+    quit()
+
+fname = str(sys.argv[1])
+
+data, labels = utils.load_dataset(fname)
+# N is 1% of the dataset 
+T = int(sys.argv[2])
+H = int(sys.argv[3])
+N = data.shape[0]
+iterations = int(sys.argv[4])
+N_init_pts = int(sys.argv[5])
+print("N_init_pts=", N_init_pts)
+
+data = np.array(data, dtype='float64')
+data = data.copy(order='C')
+
+for m in range(0, iterations):
+    print("Iteration=", m)
+    # build info reinitialized
+    t0 = time.time()
+    scores = rhfs.rhf_stream(data, T, H, N_init_pts) 
+    t1 = time.time()
+    print("AP=", average_precision_score(labels, scores))
+    print("time (whole)=", t1 - t0)
+    data, labels = utils.load_dataset(fname)
+    data =  np.array(data, dtype='float64')
+    data = data.copy(order='C')
