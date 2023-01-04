1020ea6f78196c62f9f6b20d5a836a3c045df2f5
Stefan Nesic <stefan.nesic@protonmail.com>
2023-01-04 14:48:48 +0100

    Kurtosis sum implemented in Cython

---
+++ kurtosis_sum.pyx
@@ -0,0 +1,15 @@
+import scipy.io as sio
+import scipy.stats as sstats
+import random 
+import numpy as np
+
+# sum of log(Kurtosis(X[a] + 1)) of attributes 0 to d inclusive
+cdef float kurtosis_sum(X, int d):
+    cdef float sum = 0
+    
+    # loop over the transpose matrix in order to analyze by column
+    for a in range(0, d+1):
+        # + 4 since the scipy function for kurtosis subtracts 3 so +1 +3 = 4
+        sum += np.log(sstats.stats.kurtosis(X[:,a])+4)
+        
+    return sum
