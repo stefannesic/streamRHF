dfa91fb5c5a8201feb73069bbec81852d4d942a2
Stefan Nesic <stefan.nesic@protonmail.com>
2023-01-04 14:48:49 +0100

    Incremental rhf works as well as decr/ incr kurtosis (not integrated)

--- rhf-stream/rhf_stream.pyx
+++ rhf-stream/rhf_stream.pyx
@@ -1,22 +1,25 @@
 from my_imports import np, Node, ins, rht
 
-def rhf_stream(data):
+def rhf_stream(data, n, t, h, eps):
     N = 100
-    T = 100
-    H = 5
+    T = t
+    H = h
+    
+    Node.Node.H = H
+    rht.eps = eps
     base = np.empty([N])
-    rht = None
+
+
 
     # simulating real-time 
     # construct initial tree with batch algorithm on the first N points
     for i in range(0, N):
         base[i] = data[i]
 
-    Node.Node.H = H
     tree = rht.rht(base, t=T)
 
     # update existing tree
-    for i in range(100, data.shape[0]):
+    for i in range(N, data.shape[0]):
         tree = ins.insert(tree, np.array([data[i]], np.float32))
 
     return tree
