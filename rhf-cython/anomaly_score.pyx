ccb00318a92883a17d42d4f6a00a7ec0ac91623d
Stefan Nesic <stefan.nesic@protonmail.com>
2023-01-04 14:48:48 +0100

    Reorganize code to separate cython and python. Cythonized version works

---
+++ rhf-cython/anomaly_score.pyx
@@ -0,0 +1,14 @@
+from my_imports import fi
+from libc.math cimport log
+
+cpdef float anomaly_score(rhf, int n, int[:] x):
+    cdef float sum = 0.0
+    cdef int leaf_size
+    cdef float p
+    cdef Py_ssize_t tsize = rhf.size 
+    for i in range(0, tsize):
+        # number of distinct instances in the leaf of the given instance
+        leaf_size = fi.find_instance(rhf[i], x).data.size
+        p = leaf_size / n
+        sum += log(1 / p)
+    return sum
--- setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-
-setup(
-    ext_modules = cythonize("kurtosis_sum.pyx", annotate=True, compiler_directives={'language_level' : "3"})
-)
