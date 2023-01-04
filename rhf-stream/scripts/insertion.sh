69aa91a793ed85aea19120ca3600de3c72d240ca
Stefan Nesic <stefan.nesic@protonmail.com>
2023-01-04 14:48:50 +0100

    Simplify scripts so that there's only one that takes constant and shuffled parameters

--- rhf-stream/scripts/insertion.sh
+++ rhf-stream/scripts/insertion.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-i=4;
+i=5;
 j=$#;
 pref=$1
 shift 1;
@@ -8,9 +8,13 @@ suff=$1
 shift 1;
 init=$1;
 shift 1;
+shuff=$1
+shift 1;
+const=$1
+shift 1;
 while [ $i -le $j ] 
 do
-    eval "nohup nice python3 -u insertion_script.py $1 100 5 10 ${init} > ${pref}/${1}_infres_init${init}_x10_${suff}.txt &";
+    eval "nohup nice python3 -u insertion.py $1 100 5 10 ${init} ${shuff} ${const} > ${pref}/${1}_infres_init${init}_x10_s${shuff}_c${const}_${suff}.txt &";
     i=$((i + 1));
     shift 1;
 done
