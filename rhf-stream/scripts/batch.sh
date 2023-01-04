4a64b3cee584b95f24cca74e4208d267770fa35a
Stefan Nesic <stefan.nesic@protonmail.com>
2023-01-04 14:48:50 +0100

    Insertion script takes initsamplepercent, Makefile instead of cybuild, Python3 in all scripts

--- rhf-stream/scripts/batch.sh
+++ rhf-stream/scripts/batch.sh
@@ -8,7 +8,7 @@ suff=$1
 shift 1;
 while [ $i -le $j ] 
 do
-    eval "nohup nice python -u batch_time_script.py $1 > ${pref}/${1}_infres_batch_x10_${suff}.txt &";
+    eval "nohup nice python3 -u batch_time_script.py $1 > ${pref}/${1}_infres_batch_x10_${suff}.txt &";
     i=$((i + 1));
     shift 1;
 done
